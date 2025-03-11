d4fda0f50b089818ea3056a93620540a96c519f0
mijip0320 <39548731+mijip0320@users.noreply.github.com>
2025-03-12 07:47:11 +0900

    feat : graphql mutation

--- products/products.model.js
+++ products/products.model.js
@@ -1,12 +1,49 @@
-module.exports = [
+const products = [
   {
     id: "redshoe",
     description: "Red shoe",
     price: 42.12,
+    reviews: [],
   },
   {
     id: "bluejean",
     description: "Blue Jeans",
     price: 55.55,
+    reviews: [],
   },
 ];
+
+function getAllProducts() {
+  return products;
+}
+
+function getProductsByPrice(min, max) {
+  return products.filter((product) => {
+    return product.price >= min && product.price <= max;
+  });
+}
+
+function getProductById(id) {
+  return products.find((product) => {
+    return product.id === id;
+  });
+}
+
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    description,
+    price,
+    reviews: [],
+  };
+
+  products.push(newProduct);
+  return newProduct;
+}
+
+module.exports = {
+  getAllProducts,
+  getProductsByPrice,
+  getProductById,
+  addNewProduct,
+};
--- server.js
+++ server.js
@@ -5,16 +5,18 @@ const { graphqlHTTP } = require("express-graphql");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 
-const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const typesArray = loadFilesSync("**/*", {
+  extensions: ["graphql"],
+});
+const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
 
+// const resolversArray = loadFilesSync("**/*", {
+//   extensions: ["resolvers.js"],
+// });
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
+  resolvers: resolversArray,
 });
-
-const root = {
-  products: require("./products/products.model"),
-  orders: require("./orders/orders.model"),
-};
 
 const app = express();
 
@@ -22,7 +24,6 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true, //localhost:3000/graphql로 접속하면 현재 구동되고 있는 graphql로 조회 가능
   })
 );
